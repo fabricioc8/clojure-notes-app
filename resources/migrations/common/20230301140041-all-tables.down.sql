3d570f4f84d716a103c3302e1c399c2a730d9a4e
Fabricio Cozzarolo <fabricioc_8@hotmail.com>
2023-03-02 01:29:49 -0300

    Starting migrations

---
+++ resources/migrations/common/20230301140041-all-tables.down.sql
@@ -0,0 +1,37 @@
+DROP TABLE users
+--;;
+
+DROP TABLE teams
+--;;
+
+DROP TYPE team_role
+--;;
+
+DROP TABLE team_users
+--;;
+
+DROP TABLE notes
+--;;
+
+DROP TABLE tickets
+--;;
+
+DROP TABLE ticket_messages
+--;;
+
+DROP TABLE content_pages
+--;;
+
+DROP TABLE audit_log
+--;;
+
+DROP TABLE billing_details
+--;;
+
+DROP TABLE plans
+--;;
+
+DROP TABLE subscriptions
+--;;
+
+DROP TABLE invoices
---
+++ resources/migrations/common/20230301140041-all-tables.up.sql
@@ -0,0 +1,123 @@
+
+CREATE TABLE users(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    email varchar(200) NOT NULL UNIQUE,
+    password varchar(60) NOT NULL,
+    enabled bool NOT NULL DEFAULT true,
+    api_token uuid NOT NULL DEFAULT uuid_generate_v4()
+)
+--;;
+
+CREATE TABLE teams(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name varchar(512) NOT NULL,
+    enabled bool NOT NULL DEFAULT true
+)
+--;;
+
+CREATE TYPE team_role AS ENUM (
+    'team-admin',
+    'team-editor',
+    'team-viewer'
+)
+--;;
+
+CREATE TABLE team_users(
+    team_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    role team_role NOT NULL,
+    CONSTRAINT team_users_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id),
+    CONSTRAINT team_users_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+)
+--;;
+
+CREATE TABLE notes(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    team_id uuid NOT NULL,
+    is_public bool NOT NULL DEFAULT false,
+    name varchar(200) NOT NULL,
+    content text,
+    CONSTRAINT notes_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id)
+)
+--;;
+
+CREATE TABLE tickets(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    team_id uuid NOT NULL,
+    name varchar(200) NOT NULL,
+    resolved bool NOT NULL DEFAULT false,
+    CONSTRAINT tickets_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id)
+)
+--;;
+
+CREATE TABLE ticket_messages(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    ticket_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    message varchar(1000) NOT NULL,
+    CONSTRAINT ticket_messages_ticket_id_fk FOREIGN KEY (ticket_id) REFERENCES tickets(id),
+    CONSTRAINT ticket_messages_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+)
+--;;
+
+CREATE TABLE content_pages(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    path varchar(2048) NOT NULL UNIQUE,
+    content text
+)
+--;;
+
+CREATE TABLE audit_log(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id uuid NOT NULL,
+    team_id uuid NOT NULL,
+    action text NOT NULL,
+    note text,
+    CONSTRAINT audit_log_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT audit_log_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id)
+)
+--;;
+
+CREATE TABLE billing_details(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    team_id uuid NOT NULL,
+    company_name varchar(200),
+    address varchar(1024),
+    vat varchar(100),
+    CONSTRAINT billing_details_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id)
+)
+--;;
+
+CREATE TABLE plans(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name varchar(200) NOT NULL,
+    price_usd numeric(18,2) NOT NULL,
+    price_per_user bool NOT NULL DEFAULT false,
+    public bool NOT NULL DEFAULT false,
+    team_id uuid NOT NULL,
+    max_notes integer NOT NULL,
+    max_chars integer NOT NULL,
+    max_users integer NOT NULL,
+    CONSTRAINT plan_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id)
+)
+--;;
+
+CREATE TABLE subscriptions(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    team_id uuid NOT NULL,
+    plan_id uuid NOT NULL,
+    canceled bool NOT NULL DEFAULT false,
+    CONSTRAINT subscriptions_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id),
+    CONSTRAINT subscriptions_plan_id_fk FOREIGN KEY (plan_id) REFERENCES plans(id)
+)
+--;;
+
+
+CREATE TABLE invoices(
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    team_id uuid NOT NULL,
+    amount_usd numeric(18,2),
+    subscription_id uuid NOT NULL,
+    CONSTRAINT invoices_team_id_fk FOREIGN KEY (team_id) REFERENCES teams(id),
+    CONSTRAINT invoices_subscription_id_fk FOREIGN KEY (subscription_id) REFERENCES subscriptions(id)
+)
